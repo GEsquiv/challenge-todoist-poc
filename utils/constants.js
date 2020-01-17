ce0c7f4ce294167e067c440fca01ed04678948b5
Grissell Andreina Esquivel Silva <grissell.esquivel@Grissells-MacBook-Pro.local>
2020-01-17 11:04:14 -0600

    Added utils folder

---
+++ utils/constants.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+// export const TASK_NAME = `TaskName${}`
+export const URL = `${process.env.URL}`
+export const EMAIL = `${process.env.EMAIL_ADDRESS}`
+export const PASSWORD = `${process.env.PASSWORD}`
+export const INCORRECT_EMAIL = 'esquivelgrisell@gmail.com'
+export const INCORRECT_PASSWORD = 'Star_STEY1'
+export const TASK_NAME = "hello"
+export const TASKS_NAMES = "hello1,hello2,hello3,hello4,hello5,hello6,hello7,hello8,hello9,hello10,hello11"
